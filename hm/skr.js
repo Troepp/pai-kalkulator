1d8c7ade5e9902efe15ccb5d9dc3b61d12e21eef
Troepp <frankox06@gmail.com>
2022-04-13 10:08:05 +0200

    asdf

---
+++ hm/skr.js
@@ -0,0 +1,14 @@
+function dod(){
+    document.getElementById("wysw").value += '+';
+}
+function ode(){
+    document.getElementById("wysw").value += '-';
+}
+function mnoze(){
+    document.getElementById("wysw").value += '*';
+    
+}
+function dziel(){
+    document.getElementById("wysw").value += '/';
+    
+}
